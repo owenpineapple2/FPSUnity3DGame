ab36793b871bcd2e083e4a30d9fcad91d1167725
owenpineapple2 <owenmarcantonio@gmail.com>
2025-07-17 16:03:02 -0700

    Part 2 of player projectile

--- Assets/PlayerCode.cs
+++ Assets/PlayerCode.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class PlayerCode : MonoBehaviour
 {
     public GameObject projectile; 
-
-
+    public GameObject output;
+    public float force;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,7 +17,11 @@ public class PlayerCode : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            
+            GameObject newprojectile = Instantiate(projectile);
+            newprojectile.transform.SetParent(output.transform, false);
+            newprojectile.transform.SetParent(null);
+
+            newprojectile.GetComponent<Rigidbody>().AddRelativeForce(transform.forward*force);
         }
     }
 }
